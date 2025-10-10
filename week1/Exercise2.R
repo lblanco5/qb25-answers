85f38bb4bf46b33e9ccb2788c40ce93245ba5804
Lina Blanco <lblanco5@jh.edu>
2025-10-10 11:35:10 -0400

    new

--- week1/Exercise2.R
+++ week1/Exercise2.R
@@ -1,14 +1,14 @@
 library(dplyr)
 library(tidyverse)
+setwd ('/Users/cmdb/qb25-answers/week1')
 header <- c( "chr", "start", "end", "count" )
 df_19 <-read_tsv("hg19-kc-count.bed", col_names=header)
 df_16 <-read_tsv("hg16-kc-count.bed", col_names=header)
 df_1619 <-bind_rows( hg19=df_19, hg16=df_16, .id="assembly" )
-df_plot <- 
-ggplot(df_1619, aes(x = start, y = count, color = assembly))+
+df_plot <- ggplot(df_1619, aes(x = start, y = count, color = assembly))+
   geom_line()+
   facet_wrap(~chr, scales ="free")
-ggsave("excersise2.png", plot = df_plot, width = 20, height = 10, units = "in")
+ggsave("excersise2.png", plot = df_plot)
 
 
 
