8c677aff5dd7c9493bc9fd53fb0bb0421c893b95
Lina Blanco <lblanco5@jh.edu>
2025-10-13 17:22:18 -0400

    new

--- week3/ex2.py
+++ week3/ex2.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
 import sys
+
+from numpy import append
 AF_file = open ("AF.txt", "w")
-for line in open ("BYxRM_bam/biallelic.vcf"): 
+for line in open ("/Users/cmdb/qb25-answers/week3/BYxRM_bam/biallelic.vcf"): 
     if line.startswith('#'): 
         continue
     fields= line.rstrip('\n').split('\t')
     allele_freq = fields[7].split(";")
-    AF = allele_freq [3]
-    stripped_AF = AF.strip("AF=")
-    AF_file.write(stripped_AF + "\n")
+    for line in allele_freq: 
+      if line.startswith('AF='):  
+        stripped_AF = line.strip("AF=")
+        AF_file.write(stripped_AF + "\n")
+        continue 
 AF_file.close()
     #print(AF)
 
