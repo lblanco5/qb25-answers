28b45bedf85e1bd3411340baffa2cd8b73d1f51b
Lina Blanco <lblanco5@jh.edu>
2025-12-10 21:41:08 -0500

    new

--- week10/alignment.py
+++ week10/alignment.py
@@ -8,23 +8,32 @@ from fasta import readFASTA
 # four inputs: 
 fasta_file = sys.argv[1]
 sigma_file = sys.argv[2]
-gap: int = float(sys.argv[3]) # -300
-#out_file = sys.argv[4]
+gap = float(sys.argv[3])  # -300
+out_file = sys.argv[4]
 
 input_sequences = readFASTA(open(fasta_file))
-sequence1, sequence2 = input_sequences[0][1], input_sequences[1][1]
+seq1_id, sequence1 = input_sequences[0]
+seq2_id, sequence2 = input_sequences[1]
 
 # Read the scoring matrix into a dictionary
 sigma = pd.read_csv(sigma_file, sep=r'\s+', index_col=0)
-F_matrix = np.zeros((len(sequence1) + 1 , len(sequence2) + 1))
 
-for i in range(len(sequence1) + 1):
+m = len(sequence1)
+n = len(sequence2)
+
+F_matrix = np.zeros((m+1, n+1))
+traceback_matrix = np.empty((m+1, n+1), dtype="str")
+
+for i in range(1, m+1):
     F_matrix[i, 0] = i * gap
-for j in range(len(sequence2) + 1):
-    F_matrix[0,j] = j * gap 
+    traceback_matrix[i, 0] = "v" 
+for j in range(1, n+1):
+    F_matrix[0, j] = j * gap
+    traceback_matrix[0, j] = "h"
+traceback_matrix[0,0] = "done"
 
 # options are v (vertical), d (diagonal), h (horizontal): setting the 
-traceback_matrix = np.empty((len(sequence1) + 1 , len(sequence2) + 1), dtype='str')
+#traceback_matrix = np.empty((len(sequence1) + 1 , len(sequence2) + 1), dtype='str')
 for i in range(1, len(sequence1) + 1):
     traceback_matrix[i, 0] = 'v'
 for j in range(1, len(sequence2) + 1):
@@ -54,10 +63,40 @@ i, j = len(sequence1), len(sequence2)
 while i!= 0 or j!=0: 
     sequence1_index = i - 1
     sequence2_index = j - 1   
-if traceback_matrix[i,j] == "d", 
-   s1_alignment = sequence1[sequence1_index] + s1_alignment
-   
-elif traceback_matrix[i,j] == "h", 
-    [sequence1[sequence1_index]
-else traceback_matrix[i,j] == "v"
-    [sequence1[sequence1_index]
+if traceback_matrix[i, j] == "d":
+        s1_alignment = sequence1[sequence1_index] + s1_alignment
+        s2_alignment = sequence2[sequence2_index] + s2_alignment
+        i -= 1
+        j -= 1
+elif traceback_matrix[i, j] == "h":
+        s1_alignment = "-" + s1_alignment
+        s2_alignment = sequence2[sequence2_index] + s2_alignment
+        j -= 1
+
+else:  # "v"
+        s1_alignment = sequence1[sequence1_index] + s1_alignment
+        s2_alignment = "-" + s2_alignment
+        i -= 1
+     
+#used AI for some of these portions: 
+
+num_gaps_1 = s1_alignment.count('-')
+num_gaps_2 = s2_alignment.count('-')
+
+matches = sum(a == b for a, b in zip(s1_alignment, s2_alignment))
+identity1 = matches / len(s1_alignment) * 100
+identity2 = matches / len(s2_alignment) * 100
+
+final_score = F_matrix[m, n]
+
+# write alignment
+with open(out_file, "w") as f:
+    f.write("Sequence 1 alignment:\n" + s1_alignment + "\n")
+    f.write("Sequence 2 alignment:\n" + s2_alignment + "\n")
+
+# print stats
+print("Gaps in sequence 1:", num_gaps_1)
+print("Gaps in sequence 2:", num_gaps_2)
+print("Percent identity seq1:", identity1)
+print("Percent identity seq2:", identity2)
+print("Alignment score:", final_score)
