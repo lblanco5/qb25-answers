611a916453fab2e89208887ae6ec21b2372d4bd9
Lina Blanco <lblanco5@jh.edu>
2025-12-12 17:07:34 -0500

    new

--- week11/week11.py
+++ week11/week11.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy import stats
 
 genome_size = 1_000_000
-target_coverage = 10
+target_coverage = 30
 read_length = 100 
 
 genome_coverage = np.zeros(genome_size) #1 million set of zeroes 
@@ -13,6 +13,10 @@ for x in range(number_of_reads):
     start = int(np.random.uniform(0, genome_size - read_length + 1))
     end = int(start + read_length)
     genome_coverage[start:end] += 1
+
+#used AI for some of this 
+zero_coverage_fraction = np.sum(genome_coverage == 0) / genome_size
+print("Fraction of genome with 0x coverage:", zero_coverage_fraction)
 
 max_coverage = int(max(genome_coverage))
 xs = list(range(0, max_coverage + 1))
